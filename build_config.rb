e5e6cd832ce6dda4d71ea8134d72e202369d4bcd
Uchio KONDO <udzura@pepabo.com>
2017-04-04 16:01:04 +0900

    PoC implementation

--- build_config.rb
+++ build_config.rb
@@ -1,104 +1,153 @@
-def gem_config(conf)
-  #conf.gembox 'default'
+MRuby::Build.new do |conf|
+  # load specific toolchain settings
 
-  # be sure to include this gem (the cli app)
-  conf.gem File.expand_path(File.dirname(__FILE__))
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+  end
+
+  enable_debug
+
+  # Use mrbgems
+  # conf.gem 'examples/mrbgems/ruby_extension_example'
+  # conf.gem 'examples/mrbgems/c_extension_example' do |g|
+  #   g.cc.flags << '-g' # append cflags in this gem
+  # end
+  # conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
+  # conf.gem :core => 'mruby-eval'
+  # conf.gem :mgem => 'mruby-io'
+  # conf.gem :github => 'iij/mruby-io'
+  # conf.gem :git => 'git@github.com:iij/mruby-io.git', :branch => 'master', :options => '-v'
+
+  # include the default GEMs
+  conf.gembox 'default'
+  # C compiler settings
+  # conf.cc do |cc|
+  #   cc.command = ENV['CC'] || 'gcc'
+  #   cc.flags = [ENV['CFLAGS'] || %w()]
+  #   cc.include_paths = ["#{root}/include"]
+  #   cc.defines = %w(DISABLE_GEMS)
+  #   cc.option_include_path = '-I%s'
+  #   cc.option_define = '-D%s'
+  #   cc.compile_options = "%{flags} -MMD -o %{outfile} -c %{infile}"
+  # end
+  conf.cc.flags << '-fPIC'
+
+  # mrbc settings
+  # conf.mrbc do |mrbc|
+  #   mrbc.compile_options = "-g -B%{funcname} -o-" # The -g option is required for line numbers
+  # end
+
+  # Linker settings
+  # conf.linker do |linker|
+  #   linker.command = ENV['LD'] || 'gcc'
+  #   linker.flags = [ENV['LDFLAGS'] || []]
+  #   linker.flags_before_libraries = []
+  #   linker.libraries = %w()
+  #   linker.flags_after_libraries = []
+  #   linker.library_paths = []
+  #   linker.option_library = '-l%s'
+  #   linker.option_library_path = '-L%s'
+  #   linker.link_options = "%{flags} -o %{outfile} %{objs} %{libs}"
+  # end
+
+  # Archiver settings
+  # conf.archiver do |archiver|
+  #   archiver.command = ENV['AR'] || 'ar'
+  #   archiver.archive_options = 'rs %{outfile} %{objs}'
+  # end
+
+  # Parser generator settings
+  # conf.yacc do |yacc|
+  #   yacc.command = ENV['YACC'] || 'bison'
+  #   yacc.compile_options = '-o %{outfile} %{infile}'
+  # end
+
+  # gperf settings
+  # conf.gperf do |gperf|
+  #   gperf.command = 'gperf'
+  #   gperf.compile_options = '-L ANSI-C -C -p -j1 -i 1 -g -o -t -N mrb_reserved_word -k"1,3,$" %{infile} > %{outfile}'
+  # end
+
+  # file extensions
+  # conf.exts do |exts|
+  #   exts.object = '.o'
+  #   exts.executable = '' # '.exe' if Windows
+  #   exts.library = '.a'
+  # end
+
+  # file separetor
+  # conf.file_separator = '/'
+
+  # bintest
+  # conf.enable_bintest
 end
 
-MRuby::Build.new do |conf|
-  toolchain :clang
+MRuby::Build.new('host-debug') do |conf|
+  # load specific toolchain settings
 
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+  end
+
+  enable_debug
+
+  # include the default GEMs
+  conf.gembox 'default'
+
+  # C compiler settings
+  conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
+
+  # Generate mruby debugger command (require mruby-eval)
+  conf.gem :core => "mruby-bin-debugger"
+
+  # bintest
+  # conf.enable_bintest
+end
+
+MRuby::Build.new('test') do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+  end
+
+  enable_debug
   conf.enable_bintest
-  conf.enable_debug
   conf.enable_test
 
-  gem_config(conf)
+  conf.gembox 'default'
 end
 
-MRuby::Build.new('x86_64-pc-linux-gnu') do |conf|
-  toolchain :gcc
+MRuby::Build.new('bench') do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+    conf.cc.flags << '-O3'
+  end
 
-  gem_config(conf)
+  conf.gembox 'default'
 end
 
-MRuby::CrossBuild.new('i686-pc-linux-gnu') do |conf|
-  toolchain :gcc
-
-  [conf.cc, conf.cxx, conf.linker].each do |cc|
-    cc.flags << "-m32"
-  end
-
-  gem_config(conf)
-end
-
-MRuby::CrossBuild.new('x86_64-apple-darwin14') do |conf|
-  toolchain :clang
-
-  [conf.cc, conf.linker].each do |cc|
-    cc.command = 'x86_64-apple-darwin14-clang'
-  end
-  conf.cxx.command      = 'x86_64-apple-darwin14-clang++'
-  conf.archiver.command = 'x86_64-apple-darwin14-ar'
-
-  conf.build_target     = 'x86_64-pc-linux-gnu'
-  conf.host_target      = 'x86_64-apple-darwin14'
-
-  gem_config(conf)
-end
-
-MRuby::CrossBuild.new('i386-apple-darwin14') do |conf|
-  toolchain :clang
-
-  [conf.cc, conf.linker].each do |cc|
-    cc.command = 'i386-apple-darwin14-clang'
-  end
-  conf.cxx.command      = 'i386-apple-darwin14-clang++'
-  conf.archiver.command = 'i386-apple-darwin14-ar'
-
-  conf.build_target     = 'i386-pc-linux-gnu'
-  conf.host_target      = 'i386-apple-darwin14'
-
-  gem_config(conf)
-end
-
-MRuby::CrossBuild.new('x86_64-w64-mingw32') do |conf|
-  toolchain :gcc
-
-  [conf.cc, conf.linker].each do |cc|
-    cc.command = 'x86_64-w64-mingw32-gcc'
-  end
-  conf.cxx.command      = 'x86_64-w64-mingw32-g++'
-  conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
-
-  conf.exts do |exts|
-    exts.object = '.obj'
-    exts.executable = '.exe'
-    exts.library = '.lib'
-  end
-
-  conf.build_target     = 'x86_64-pc-linux-gnu'
-  conf.host_target      = 'x86_64-w64-mingw32'
-
-  gem_config(conf)
-end
-
-MRuby::CrossBuild.new('i686-w64-mingw32') do |conf|
-  toolchain :gcc
-
-  [conf.cc, conf.linker].each do |cc|
-    cc.command = 'i686-w64-mingw32-gcc'
-  end
-  conf.cxx.command      = 'i686-w64-mingw32-g++'
-  conf.archiver.command = 'i686-w64-mingw32-gcc-ar'
-
-  conf.exts do |exts|
-    exts.object = '.obj'
-    exts.executable = '.exe'
-    exts.library = '.lib'
-  end
-
-  conf.build_target     = 'i686-pc-linux-gnu'
-  conf.host_target      = 'i686-w64-mingw32'
-
-  gem_config(conf)
-end
+# Define cross build settings
+# MRuby::CrossBuild.new('32bit') do |conf|
+#   toolchain :gcc
+#
+#   conf.cc.flags << "-m32"
+#   conf.linker.flags << "-m32"
+#
+#   conf.build_mrbtest_lib_only
+#
+#   conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
+#
+#   conf.test_runner.command = 'env'
+#
+# end
--- mrblib/simulacre.rb
+++ mrblib/simulacre.rb
@@ -1,7 +1,18 @@
-def __main__(argv)
-  if argv[1] == "version"
-    puts "v#{Simulacre::VERSION}"
-  else
-    puts "Hello World"
+module Simulacre
+  def self.clock_gettime(clk_id, ts)
+    if rand(2) == 0
+      Simulacre.super
+    else
+      ts.sec = rand(10000)
+    end
+    return true
   end
 end
+
+# def __main__(argv)
+#   if argv[1] == "version"
+#     puts "v#{Simulacre::VERSION}"
+#   else
+#     puts "Hello World"
+#   end
+# end
